66ab3af5f42f22c52e5a467da0d65309f4c5c99b
JP BRUNET <55714777+jpbrunet@users.noreply.github.com>
2021-06-16 13:55:05 +0200

    Implement code for P2C4

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,27 +1,53 @@
 <script>
 export default {
-	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"]
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+  data() {
+    return {
+      onSale: true
+    }
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9 ).toFixed(2)
+      } else {
+        return this.price
+      }
+    }
+  },
+  beforeMount() {
+    const today = new Date.getDate()
+
+    if (today % 2 === 0) {
+      this.onSale = false
+    }
+  }
 }
 </script>
 
 <template>
-	<div class="menu-item">
-		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
-		<div>
-			<h3>{{ name }}</h3>
-			<p>Prix : {{ price }}</p>
-			<p v-if="inStock">En stock</p>
-			<p v-else>En rupture de stock</p>
-			<div>
-				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
-				<input v-model.number="quantity" id="add-item-quantity" type="number" />
-				<button @click="addToShoppingCart(quantity)">
-					Ajouter au panier
-				</button>
-			</div>
-		</div>
-	</div>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt"/>
+    <div>
+      <h3>{{ name }}</h3>
+      <p v-if="onSale">Prix : <span class="priceSale">{{ price }}</span> | {{ generatedPrice }} <span> Solde -10%</span></p>
+      <p v-else>Prix : {{ price }}</p>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        <input v-model.number="quantity" id="add-item-quantity" type="number"/>
+        <button @click="addToShoppingCart(quantity)">
+          Ajouter au panier
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.priceSale {
+  text-decoration: line-through;
+}
+</style>
